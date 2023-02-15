4c532907a8ab3013d75b02891115d1f474edd89b
Joe Morina <121312601+morinajc@users.noreply.github.com>
2023-02-14 21:25:01 -0500

    Update PurpleAirAPI.R

--- PurpleAirAPI.R
+++ PurpleAirAPI.R
@@ -2,27 +2,27 @@ library(readr)
 library(lubridate) 
 library(httr) 
 
-#API keys assigned by PurpleAir support
+# API keys assigned by PurpleAir support
 read_key <- "Read Key "
 write_key <- "Write Key"
 
-#end date and start date (API format is UNIX GMT)
+# End date and start date (API format is UNIX GMT)
 start_date <- as.POSIXct("2020-01-01")
 end_date <- as.POSIXct("2023-02-14")
 
 while(start_date <= end_date) {
-  #define end of 2 week period, max request period for 1hr data
+  # Define end of 2 week period, max request period for 1hr data
   end_period <- start_date + days(14)
   
-  #format start and end times as UNIX timestamps
+  # Format start and end times as UNIX timestamps
   start_timestamp <- as.numeric(start_date)
   end_timestamp <- as.numeric(end_period)
   
-  #substitute start_timestamp and end_timestamp into request URL
-  #replace "sensorindex" with your actual sensor index
+  # Substitute start_timestamp and end_timestamp into request URL
+  # Replace "sensorindex" with your actual sensor index
   url <- paste0("https://api.purpleair.com/v1/sensors/sensorindex/history/csv?start_timestamp=", start_timestamp, "&end_timestamp=", end_timestamp, "&average=60&fields=pm2.5_cf_1_a%2C%20pm2.5_cf_1_b%2Cpm2.5_atm_a%2C%20pm2.5_atm_b%2Cpm2.5_alt_a%2Cpm2.5_alt_b%2Chumidity%2Ctemperature%2Cpressure%2Cuptime%2Crssi%2Cpa_latency%2Cmemory")
   
-  #send API request and write to text file
+  # Send API request and write to text file
   data <- GET(url,add_headers('X-API-Key'=read_key))
   data <- content(data, "raw")
   writeBin(data, paste("sensorindex",start_timestamp,end_timestamp,".txt", sep="_"))
@@ -32,12 +32,12 @@ while(start_date <= end_date) {
   Sys.sleep(60)
 }
 
-## Merging files
+# Merging files
 listfile <- list.files("/path/to/directory", pattern = "txt",full.names = T, recursive = TRUE)
 
 head(listfile)
 
-#combined all the text files in listfile and store in dataframe 'Data'
+# Combined all the text files in listfile and store in dataframe 'Data'
 for (i in 1:length(listfile)){
   if(i==1){
     assign(paste0("Data"), read.table(listfile[i],header = TRUE, sep = ","))
@@ -50,5 +50,5 @@ for (i in 1:length(listfile)){
     rm(list = ls(pattern = "Test"))
   }
 }
-
+# Save dataframe as csv file
 write.csv(Data, "File Name.csv")
